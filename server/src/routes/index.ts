5da1c9342dfe3dd31517c39bc00701c0ea3db066
lexusnealy <lexus.elisha1@gmail.com>
2025-06-02 18:26:12 -0400

    code added from scratch for github actions

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -11,10 +11,8 @@ import apiRoutes from './api/index.js';
 router.use('/api', apiRoutes);
 
 // serve up react front-end in production
-if (process.env.NODE_ENV === 'production') {
-  router.use((_req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
-  });
-}
+router.use((_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+});
 
 export default router;
--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,12 +3,11 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    const model = models[modelName];
-const collections = await model?.db?.db?.listCollections({
-  name: model?.collection?.name,
-}).toArray();
+    let modelExists = await models[modelName].db.db.listCollections({
+      name: collectionName
+    }).toArray()
 
-if (collections && collections.length) {
+    if (modelExists.length) {
       await db.dropCollection(collectionName);
     }
   } catch (err) {
